5a556f70d07a6a6168ae1f0a5a8f9174e7f13278
MoBsTe <valery.lobach@gail.com>
2023-03-09 20:34:40 +0100

    clean code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const input = document.querySelector('.input_text');
-const inputSearch = document.querySelector('.search_input')
+const inputSearch = document.querySelector('.search_input');
 const btnAdd = document.querySelector('button');
 const listItem = document.querySelector('.list_items');
 const darkMode = document.querySelector('.dark-mode');
@@ -20,7 +20,7 @@ btnAdd.addEventListener('click', (e) => {
     text.textContent = input.value;
     div.appendChild(text);
 
-    //create div pen
+    //create div closet
     let divClose = document.createElement('div');
     divClose.innerHTML = '<i class="fa fa-close"></i>';
     divClose.classList.add('closet');
@@ -48,9 +48,9 @@ btnAdd.addEventListener('click', (e) => {
 // remove div.list_item
 function removel(e) {
     const item = e.target;
-    const todo = item.closest('.list_item')
+    const todo = item.closest('.list_item');
     todo.remove();
-}
+};
 
 
 // change Text contant 
@@ -63,12 +63,12 @@ function change_contant(e) {
     change.value = textDiv.textContent;
     change.classList.add('input_change');
     todo.appendChild(change);
-    const divPen = todo.querySelector('.pen')
+    const divPen = todo.querySelector('.pen');
     divPen.style.display = 'none';
     const divConfirm = todo.querySelector('.confirm');
     divConfirm.style.display = 'block';
     divConfirm.addEventListener('click', confirmbtn);
-}
+};
 
 
 // confirm btn 
@@ -80,11 +80,11 @@ function confirmbtn(e) {
     text.textContent = change.value;
     change.remove();
     text.style.display = 'block';
-    const divConfirm = confirm.querySelector('.confirm')
+    const divConfirm = confirm.querySelector('.confirm');
     divConfirm.style.display = 'none';
-    const divPen = confirm.querySelector('.pen')
+    const divPen = confirm.querySelector('.pen');
     divPen.style.display = 'block';
-}
+};
 
 
 // search input
@@ -98,10 +98,10 @@ inputSearch.addEventListener('input', () => {
             element.style.display = 'flex';
         } else {
             element.style.display = 'none';
-        }
-    })
+        };
+    });
     console.log(inputSearch.value);
-})
+});
 
 
 
@@ -109,12 +109,4 @@ inputSearch.addEventListener('input', () => {
 function changeMode() {
     const element = document.body;
     element.classList.toggle("dark-mode");
-}
-
-
-
-
-
-// если значение input уже добавлено, вывести предупреждение
-// поиск 
-
+};
